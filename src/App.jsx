b145dd3f0928ea137d9a359f08fa850b0cc09aa3
William <wimoca95@gmail.com>
2023-10-06 20:20:37 -0400

    Ruta default para rutas invalidas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 import MusicPlayer from "./components/MusicPlayer";
@@ -18,7 +18,10 @@ function App() {
           element={<SearchPage />}
         />
         <Route path="/aKappa-proyecto" element={<HomePage />} />
-        <Route path="*" element={<HomePage />} />
+        <Route
+          path="*"
+          element={<Navigate to={"/aKappa-proyecto"} replace />}
+        />
       </Routes>
     </>
   );
