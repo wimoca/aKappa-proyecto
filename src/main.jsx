81ad4f832ee01afb968f21e3de1aa65ff0eae273
William <wimoca95@gmail.com>
2023-10-06 21:01:25 -0400

    spa update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter basename="/aKappa-proyecto">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
