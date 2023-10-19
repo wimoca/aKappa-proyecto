78fa48f6b7e5ad7c32113ba3cfb72fa54967757a
William <wimoca95@gmail.com>
2023-10-19 17:05:39 -0400

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Maurizm.github.io/KappamusicFrontend",
+  base: "https://wimoca95.github.io/aKappa-proyecto",
 });
