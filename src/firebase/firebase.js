f5c36629420e7599283de7597b587180b60e722d
William <wimoca95@gmail.com>
2023-10-22 23:08:35 -0400

    fix deploy?

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+//export const auth = getAuth(app);
+//export const db = getFirestore(app);
