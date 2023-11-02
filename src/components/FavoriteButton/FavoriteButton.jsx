ea48fc3b1d741596ccf28cdf133632a6f20377e6
William <wimoca95@gmail.com>
2023-11-02 11:51:32 -0400

    Arreglado bug: Alerta de agregar o remover de favoritos

--- src/components/FavoriteButton/FavoriteButton.jsx
+++ src/components/FavoriteButton/FavoriteButton.jsx
@@ -5,13 +5,14 @@ import { COLORS } from "../../colors/colors";
 import { updateFavorites } from "../../firebase/hooks/updateFavorites";
 import { removeFavorites } from "../../firebase/hooks/removeFavorites";
 import { getUserData } from "../../firebase/hooks/getUserData";
-import { Button } from "@mui/material";
+import { Alert, Button, Snackbar } from "@mui/material";
 import { isEmpty } from "../MusicPlayer/hooks/isEmpty";
 
 function FavoriteButton({ song }) {
   const { userData, setUserData, currentSong } = useContext(playerContext);
   const [data, setData] = useState(userData);
   const [track, setTrack] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   const onHandleClick = async (song, action) => {
     if (isEmpty(song)) {
@@ -19,13 +20,21 @@ function FavoriteButton({ song }) {
     }
     if (action == "add") {
       await updateFavorites(song);
+      setIsOpen(true);
       console.log("add");
     } else {
       await removeFavorites(song);
+      setIsOpen(true);
       console.log("remove");
     }
   };
 
+  const onHandleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      setIsOpen(false);
+    }
+    setIsOpen(false);
+  };
   // const onHandleClickAdd = async (song) => {
   //   await updateFavorites(song).then(() => getUserData(setUserData));
   //   console.log("add");
@@ -45,15 +54,43 @@ function FavoriteButton({ song }) {
 
   if (userData[0]["favorites"].some((item) => item.id === song.id)) {
     return (
-      <Button onClick={() => onHandleClick(song, "remove")}>
-        <BsStarFill color={COLORS.accentColor} size={22} />
-      </Button>
+      <div>
+        <Snackbar
+          open={isOpen}
+          autoHideDuration={2500}
+          onClose={onHandleClose}
+          //message="Note archived"
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          //action={action}
+        >
+          <Alert severity="success" onClose={onHandleClose}>
+            Añadido a Favoritos
+          </Alert>
+        </Snackbar>
+        <Button onClick={() => onHandleClick(song, "remove")}>
+          <BsStarFill color={COLORS.accentColor} size={22} />
+        </Button>
+      </div>
     );
   } else {
     return (
-      <Button onClick={() => onHandleClick(song, "add")}>
-        <BsStar color={COLORS.accentColor} size={22} />
-      </Button>
+      <div>
+        <Snackbar
+          open={isOpen}
+          autoHideDuration={2500}
+          onClose={onHandleClose}
+          //message="Note archived"
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          //action={action}
+        >
+          <Alert severity="warning" onClose={onHandleClose}>
+            Removido de Favoritos
+          </Alert>
+        </Snackbar>
+        <Button onClick={() => onHandleClick(song, "add")}>
+          <BsStar color={COLORS.accentColor} size={22} />
+        </Button>
+      </div>
     );
   }
 }
